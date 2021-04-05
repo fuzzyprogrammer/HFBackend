4677c7b30d2ae63b768881381c7c87f9f7b35761
fuzzyprogrammer <fuzzyprogrammerincode@gmail.com>
2021-04-05 17:40:20 +0530

    updated

--- database/migrations/2021_03_01_044826_create_hf_users_table.php
+++ database/migrations/2021_03_01_044826_create_hf_users_table.php
@@ -19,7 +19,7 @@ class CreateHfUsersTable extends Migration
             $table->unsignedBigInteger('jamath_id')->nullable();
             $table->unsignedBigInteger('parent_id')->nullable();
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('email',191)->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
